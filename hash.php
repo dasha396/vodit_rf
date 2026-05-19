61cb69e253b720c04c7a8869c9bd6a44d5dae8c0
даша и даша <lovedasa586@gmail.com>
2026-05-19 06:24:56 +0300

    gg

--- hash.php
+++ hash.php
@@ -6,3 +6,5 @@ $hash = password_hash($password, PASSWORD_DEFAULT);
 echo "Пароль: " . $password . "<br>";
 echo "Хэш: " . $hash;
 ?>
+/*<link rel="stylesheet" href="../assets/bootstrap/css/bootstrap.min.css">*/
+//<script src="../assets/bootstrap/js/bootstrap.bundle.min.js"><script>//
